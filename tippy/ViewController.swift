0ac2db865b9107e2a93bb1ef3f62d7770bc03fbf
Lixuan Lang <lixuanlang@lixuandembp.attlocal.net>
2017-12-29 19:13:15 -0800

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Lixuan Lang on 12/29/17.
+//  Copyright © 2017 Lixuan Lang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
